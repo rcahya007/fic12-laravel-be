d6536a875e6aa9211beb9762b88400e17a246870
RendyCahyaE <rendycahya6@gmail.com>
2024-01-19 00:45:12 +0700

    create api user, category, products

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json([
+            'message' => 'Success',
+            'data' => $categories,
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// User
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+
+// Category
+Route::get('/categories', [CategoryController::class, 'index']);
+
+// Products
+Route::get('/products', [ProductController::class, 'index']);
